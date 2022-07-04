d36bc3ecb05f75a78b3317932dcb84067d68fd72
ockapy <maxime.rodriguez.st@gmail.com>
2022-07-04 11:39:52 +0200

    auto item array

--- parser.js
+++ parser.js
@@ -3,8 +3,11 @@ const puppeteer = require('puppeteer');
 
 async function GetData(arg) {
     let input = String(arg);
+    const customargs = [
+        `--start-maximized`
+    ]
     try {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: false, args: customargs,});
         const page = await browser.newPage();
         await page.goto('https://escapefromtarkov.fandom.com/wiki/Special:Search?fulltext=1&query=&scope=internal&contentType=&ns%5B0%5D=0')
         await page.waitForSelector('input[name=query]');
@@ -13,12 +16,12 @@ async function GetData(arg) {
         await page.$eval('button[type="submit"]', e => e.click());
         await page.waitForSelector('article');
         console.log("Page 2 Loaded")
-        await page.evaluate(() => document.querySelector('article > h1 > a').click());
+        await page.evaluate(() => document.querySelector('article > h3 > a').click());
         await page.waitForSelector('table#va-infobox0');
         console.log("Page 3 Loaded");
         try {
             await page.evaluate(() => document.querySelector('body > div:nth-child(11) > div > div > div._1r08nyekFdI7_2d8r3AIBf > div.NN0_TB_DIsNmMHgJWgT7U.XHcr6qf5Sub2F2zBJ53S_').click());
-        } catch (e) { print(e) }
+        } catch (e) { }
         await page.waitForSelector('#va-infobox0 > tbody > tr.va-infobox-row-mainimage > td')
         const element = await page.$('#va-infobox0 > tbody > tr.va-infobox-row-mainimage > td')
         await element.screenshot({ path: 'screenshot.png' })
@@ -36,93 +39,7 @@ async function GetData(arg) {
 }
 
 
-async function Display(arg) {
-    let elements = await GetData()
-    let count = 0
-    let Dataset = []
-
-
-    for (let i = 0; i <= elements.length; i++) {
-
-    }
-
-    let Data = {
-        caliber: '',
-        name: '',
-        damage: '',
-        PP: '',
-        AD: '',
-        Accuracy: '',
-        Recoil: '',
-        FChance: '',
-        L: '',
-        H: '',
-        P1: '',
-        P2: '',
-        P3: '',
-        P4: '',
-        P5: '',
-        P6: '',
-    }
-
-    let match = []
-    match.push(0)
-    match.push(Data)
-
-
-    elements.forEach(element => {
-
-        let ammo = element;
-        let meta = JSON.stringify(ammo)
-        let array = String(meta).replace(/\\t/g, '$')
-        let metadata = array.split('$')
-        let name = metadata[0].toString().replace('\"', '')
-        name = name.split(' ').join('')
-
-
-        count = 0
-
-        for (let i in arg) {
-
-            metadata[0].toLowerCase().includes(arg[i].toLowerCase()) ? count++ : false;
-
-        }
-
-        if (count > match[0]) {
-
-            count = 0
-
-
-            Data = {
-                caliber: metadata[0],
-                name: name,
-                damage: metadata[1],
-                PP: metadata[2],
-                AD: metadata[3],
-                Accuracy: metadata[4],
-                Recoil: metadata[5],
-                FChance: metadata[6],
-                L: metadata[7],
-                H: metadata[8],
-                P1: metadata[9],
-                P2: metadata[10],
-                P3: metadata[11],
-                P4: metadata[12],
-                P5: metadata[13],
-                P6: metadata[14],
-
-
-
-            }
-            Dataset.push(Data)
-            match[1] = metadata
-
-        }
-    });
-
-
-
-    return Dataset
+async function Sort(data){
 
 }
 
@@ -167,24 +84,7 @@ async function Find(search) {
         console.log(meta)
     }
 
-
-
-
-
 }
 
 
-
-
-
-async function GetScreenshot() {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://static.wikia.nocookie.net/escapefromtarkov_gamepedia/images/5/55/CustomsLargeExpansionGloryMonki.png/revision/latest?cb=20210224211032')
-    const data = await page.screenshot({ path: 'screenshot.png' })
-    browser.close();
-    return data
-}
-
-
-Find('prapor')
+Find('vog')
